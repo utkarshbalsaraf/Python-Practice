efc28732642caa531b12f7e80fbabd8e650be024
utkarshbalsaraf <153362332+utkarshbalsaraf@users.noreply.github.com>
2024-08-17 15:38:05 +0530

    operators

---
+++ 03_operators.py
@@ -0,0 +1,60 @@
+# arithmatic operators (+,-,*,/,%,**)
+a=10
+b=3
+c=None
+
+print('arithmatic operators (+,-,*,/,%,**)')
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# relational operators (==, !=, >,<, >=,<=)
+print('\n\nrelational operators (==, !=, >,<, >=,<=)')
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+# assignment operators ( = ,+=,-=,/=,*=,%=,**=)
+print('\n\nassignment operators ( = ,+=,-=,/=,*=,%=,**=)')
+c = a+b
+print(c)
+c +=a
+print(c)
+c -= a
+print(c)
+c/= b
+print(c)
+c*=a
+print(c)
+c%=b
+print(c)
+c**=a
+print(c)
+
+# Logical operator ( not, and , or)
+print('\n\nLogical operator ( not, and , or)')
+a=10
+b=20
+print(not True)
+print(not (a<b))
+
+val1=True
+val2=False
+val3=True
+val4=False
+
+print(val1 and val2)
+print(val1 and val3)
+print(val2 and val4)
+
+print(val1 or val2)
+print(val1 or val3)
+print(val2 or val4)
+
+
