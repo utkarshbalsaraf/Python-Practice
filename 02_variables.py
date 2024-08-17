815872585c9f357743a01be39756abc1675d5a89
utkarshbalsaraf <153362332+utkarshbalsaraf@users.noreply.github.com>
2024-08-17 15:13:50 +0530

    varaibles

---
+++ 02_variables.py
@@ -0,0 +1,14 @@
+name="Utkarsh";
+a=20;
+b=30;
+c=41.234
+d=True;
+print(name);
+print(a+b);
+print(a+c);
+print('My name is '+name+ ' and my name is',a)
+print(type(a))
+print(type(name))
+print(type(c))
+print(type(d))
+
