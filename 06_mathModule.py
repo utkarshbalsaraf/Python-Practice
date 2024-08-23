467a6c790fac7f7867e23b55c8b2e9417fb95d01
utkarshbalsaraf <153362332+utkarshbalsaraf@users.noreply.github.com>
2024-08-24 03:16:50 +0530

    math

--- 06_mathModule.py
+++ 06_mathModule.py
@@ -1,6 +1,6 @@
 import math
 
-x=5.4
+x = 5.4
 # print(math.pi)
 # print(math.e)
 #
@@ -8,10 +8,15 @@ x=5.4
 # print(math.ceil(x))
 # print(math.floor(x))
 
-radius = float(input("Enter the radius of circle"))
+# radius = float(input("Enter the radius of circle"))
 
-circumference= 2*math.pi * radius
-print(f"Circumference of circle is : {round(circumference,2)}")
-
-area = math.pi * radius ** 2
-print(f"Area of circle is : {round(area,2)} cm.sq")
+# circumference = 2 * math.pi * radius
+# print(f"Circumference of circle is : {round(circumference, 2)}")
+#
+# area = math.pi * radius ** 2
+# print(f"Area of circle is : {round(area, 2)} cm.sq")
+y = 34
+z = 84
+print(max(x, y, z))
+print(min(x, y, z))
+print(round(math.sqrt(7), 2))
